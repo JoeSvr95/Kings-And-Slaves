c75d9ae1f40869d69b314a1652c58f6e5fb7f4db
Joe Saverio <joesaverio@hotmail.com>
2018-08-20 18:02:58 -0500

    Archivos de Tiled de los mapas

---
+++ Tiled/Mapas/test-tiled.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="WhatsApp Image 2018-07-22 at 9.40.02 PM" tilewidth="16" tileheight="16" tilecount="2193" columns="51">
+ <image source="../../Downloads/WhatsApp Image 2018-07-22 at 9.40.02 PM.jpeg" width="826" height="701"/>
+</tileset>
