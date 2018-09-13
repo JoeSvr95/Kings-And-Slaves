93854a8bee00cce6b6cf635a718eb92fe88895cd
JoeSvr95 <joesaverio@hotmail.com>
2018-09-13 04:01:56 -0500

    Opción de controles

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 public class MainMenu : MonoBehaviour {
 
 	public LevelChanger lvlChanger;
+	public GameObject controls;
 
 	public void PlayGame(){
 		lvlChanger.FadeToLevel(1);
 	}
 
 	public void QuitGame(){
-		Debug.Log("Quit!");
 		Application.Quit(); 
+	}
+
+	public void ShowControls(){
+		controls.SetActive(true);
+	}
+
+	public void HideControls(){
+		controls.SetActive(false);
 	}
 
 }
